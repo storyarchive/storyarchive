a95964a4a54dbb0a2fcc9d2b075be869146d64b3
Txuritan <txurtian@yahoo.com>
2018-02-24 00:33:58 -0500

    Internals based off @svenstaro/rust-web-boilerplate

--- src/hub.rs
+++ src/hub.rs
@@ -1,12 +1,90 @@
-use std::vec::Vec;
+use std::collections::HashMap;
 
-use rocket::Route;
+use rocket::{Route, State};
+use rocket_contrib::Template;
+
+use config::storyarchive::StoryArchiveConfig;
+use config::theme::ThemeConfig;
 
 pub fn routes() -> Vec<Route> {
-    routes![home]
+    routes![
+        home,
+        login, logout, register
+    ]
 }
 
 #[get("/")]
-pub fn home() -> &'static str {
-    "Hello from Hub"
+pub fn home(
+    storyarchive_config: State<StoryArchiveConfig>,
+    theme_config: State<ThemeConfig>,
+) -> Template {
+    let mut context = HashMap::new();
+    context.insert("path", "");
+
+    Template::render(
+        format!(
+            "{}/{}/{}",
+            storyarchive_config.general.themes_dir,
+            storyarchive_config.general.theme,
+            theme_config.path.hub.home
+        ),
+        &context
+    )
+}
+
+#[get("/login")]
+pub fn login(
+    storyarchive_config: State<StoryArchiveConfig>,
+    theme_config: State<ThemeConfig>,
+) -> Template {
+    let mut context = HashMap::new();
+    context.insert("path", "");
+
+    Template::render(
+        format!(
+            "{}/{}/{}",
+            storyarchive_config.general.themes_dir,
+            storyarchive_config.general.theme,
+            theme_config.path.hub.login
+        ),
+        &context
+    )
+}
+
+#[get("/logout")]
+pub fn logout(
+    storyarchive_config: State<StoryArchiveConfig>,
+    theme_config: State<ThemeConfig>,
+) -> Template {
+    let mut context = HashMap::new();
+    context.insert("path", "");
+
+    Template::render(
+        format!(
+            "{}/{}/{}",
+            storyarchive_config.general.themes_dir,
+            storyarchive_config.general.theme,
+            theme_config.path.hub.home
+        ),
+        &context
+    )
+}
+
+#[get("/register")]
+pub fn register(
+    storyarchive_config: State<StoryArchiveConfig>,
+    theme_config: State<ThemeConfig>,
+) -> Template {
+    let mut context = HashMap::new();
+    context.insert("path", "");
+
+    Template::render(
+        format!(
+            "{}/{}/{}",
+            storyarchive_config.general.themes_dir,
+            storyarchive_config.general.theme,
+            theme_config.path.hub.register
+        ),
+        &context
+    )
 }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,63 @@
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
 
+
+#[macro_use(bson, doc)]
+extern crate bson;
 extern crate config as cconfig;
+extern crate mongodb;
 extern crate rocket;
 extern crate rocket_contrib;
 extern crate serde;
-#[macro_use] extern crate serde_derive;
+#[macro_use]
+extern crate serde_derive;
 extern crate toml;
+
 
 mod config;
 mod helpers;
 mod hub;
-mod theme;
+
+
+use std::string::String;
+use std::sync::Arc;
+
+use config::storyarchive::StoryArchiveConfig;
+use config::theme::ThemeConfig;
+use helpers::db::init_db;
+
+use rocket_contrib::Template;
+
+
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+
+fn read() -> Result<rocket::Rocket, String> {
+    let config = StoryArchiveConfig::new()
+        .map_err(|e| e.to_string())?;
+
+    let theme_config = ThemeConfig::new(
+        config.general.theme.clone(),
+        config.general.themes_dir.clone(),
+    ).map_err(|e| e.to_string())?;
+
+    let (client, db) = init_db(
+        &config.database.database_url.clone(),
+        &config.database.database_name.clone(),
+    ).map_err(|e| e.to_string())?;
+
+    let rocket = rocket::ignite()
+        .mount("/", hub::routes())
+        .attach(Template::fairing())
+        .manage(Arc::clone(&client))
+        .manage(Arc::clone(&db))
+        .manage(config)
+        .manage(theme_config);
+
+    Ok(rocket)
+}
 
 fn main() {
-    let config = config::StoryArchiveConfig::new().unwrap();
-    let theme_config = theme::ThemeConfig::new(&config).unwrap();
-
-    rocket::ignite()
-        .mount("/", hub::routes())
-        .launch();
+    let rocket = read().unwrap();
+    rocket.launch();
 }
