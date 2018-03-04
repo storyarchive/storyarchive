70e8816d874267511673ccc592a611195a657b4e
Txuritan <txurtian@yahoo.com>
2018-03-03 23:42:09 -0500

    Updated story model with MongoDB ObjectIds and added documentation

--- src/models/story.rs
+++ src/models/story.rs
@@ -1,11 +1,26 @@
 use std::collections::HashMap;
 use std::string::String;
 
+use bson::Document;
+use bson::oid::ObjectId;
+
+/// The story and its information
+/// # Fields
+///   * ```chapters```: A hashmap containing each chapter by its ChapterId and its MongoDB ObjectId
+///     (see ```Chapter``` struct)
+///   * ```meta```: Contains overall information on the story and its chapters
+///     (see ```StoryMeta``` struct)
 pub struct Story {
-    pub chapters: HashMap<u32, Chapter>,
+    pub chapters: Option<HashMap<u32, ObjectId>>,
     pub meta: StoryMeta,
 }
 
+/// The story's overall information
+/// # Fields
+///   * ```favorites```: Stores the story's favorite count
+///   * ```follows```: Stores the story's follow count
+///   * ```views```: Stores the story's view count
+///   * ```words```: Stores the story's total word count
 pub struct StoryMeta {
     pub favorites: u32,
     pub follows: u32,
@@ -13,15 +28,51 @@ pub struct StoryMeta {
     pub words: u32,
 }
 
+/// The story's tags
+/// # Fields
+pub struct StoryTags {
+
+}
+
+/// The chapter and its information
+/// # Fields
+///   * ```content```: Contains the main body of a chapter
+///     (see ```ChapterContent``` struct)
+///   * ```meta```: Contains overall information on the chapter
+///     (see ```ChapterMeta``` struct)
 pub struct Chapter {
+    pub content: ChapterContent,
     pub meta: ChapterMeta,
 }
 
+/// The raw and rendered forms of the chapter's markdown
+/// # Fields
+///   * ```raw```: Contains the raw markdown of the chapter
+///     (see ```ChapterContentRaw``` struct)
+///   * ```rendered```: Contains the rendered markdown of the chapter
+///     (see ```ChapterContentRendered``` struct)
 pub struct ChapterContent {
+    pub raw: ChapterContentRaw,
+    pub rendered: ChapterContentRendered,
+}
+
+/// The chapter's raw non-rendered markdown
+/// # Fields
+pub struct ChapterContentRaw {
     pub authors_note: String,
     pub body: String,
 }
 
+/// The chapter's rendered markdown
+/// # Fields
+pub struct ChapterContentRendered {
+    pub authors_note: String,
+    pub body: String,
+}
+
+/// The overall information of the chapter
+/// # Fields
+///   * ```words```: Stores the chapter's total word count
 pub struct ChapterMeta {
     pub words: u32,
 }
