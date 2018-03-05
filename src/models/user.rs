5f03fb524b1993e20398f8538d8bf656def0dfc8
Txuritan <txurtian@yahoo.com>
2018-03-04 19:24:02 -0500

    Updated user model with MongoDB ObjectIds and added documentation

--- src/models/user.rs
+++ src/models/user.rs
@@ -1,19 +1,34 @@
 use std::string::String;
 
 use bson::Document;
+use bson::oid::ObjectId;
 
 use models::story::Story;
 
+/// A user and its information and settings
+/// # Fields
+///   * ```authors_favorite```: A vector of ObjectIds of the user's favorite users
+///   * ```authors_following```: A vector of ObjectIds of the users that the user is following
+///   * ```description```: The biography of the user
+///   * ```email```: The user's email address
+///   * ```favorite```: A vector of ObjectIds of users that are favorites of the user
+///   * ```followers```: A vector of ObjectIds of users that follow the user
+///   * ```rank```: The users rank
+///   * ```stories```: A vector of ObjectIds of stories that the user has
+///   * ```stories_favorite```: A vector of ObjectIds of stories that the user favorites
+///   * ```stories_following```: A vector of ObjectIds of stories that the user follows
+///   * ```username```: The visible username of the user
 pub struct User {
-    pub authors_favorite: Vec<User>,
-    pub authors_following: Vec<User>,
+    pub authors_favorite: Vec<ObjectId>,
+    pub authors_following: Vec<ObjectId>,
     pub description: String,
     pub email: String,
+    pub favorite: Vec<ObjectId>,
+    pub followers: Vec<ObjectId>,
     pub rank: u32,
-    // Figure out how to use MongoDB ObjecID
-    pub stories: Vec<Story>,
-    pub stories_favorite: Vec<Story>,
-    pub stories_following: Vec<Story>,
+    pub stories: Vec<ObjectId>,
+    pub stories_favorite: Vec<ObjectId>,
+    pub stories_following: Vec<ObjectId>,
     pub username: String,
 }
 
