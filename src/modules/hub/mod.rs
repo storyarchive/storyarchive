30d74185db9663b4cd261ff67d317f9b68174309
Ian Cronkright <txurtian@yahoo.com>
2018-03-08 23:17:06 +0000

    All hub pages

--- src/modules/hub/mod.rs
+++ src/modules/hub/mod.rs
@@ -1,13 +1,21 @@
+pub mod about;
 pub mod home;
 pub mod login;
 pub mod logout;
+pub mod privacy;
 pub mod register;
+pub mod support;
+pub mod terms;
 
 use rocket::Route;
 
 pub fn routes() -> Vec<Route> {
     routes![
         home::home,
+        about::about,
         login::login, logout::logout, register::register,
+        privacy::privacy,
+        support::support,
+        terms::terms,
     ]
 }
